a21ed441fe29d273f96a20978bb858e7dc376a23
temejin <93370315+temejin@users.noreply.github.com>
2022-10-10 19:19:54 +0300

    navigointipalkki ja reititykset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
+  root 'breweries#index'
+  get 'kaikki_bisset', to: 'beers#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
