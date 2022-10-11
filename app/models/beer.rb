11897f24bbb9fd6eb5fa93e5af2ae2850a577a45
temejin <93370315+temejin@users.noreply.github.com>
2022-10-11 19:58:16 +0300

    tehtävä 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,16 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    if self.ratings.empty?
+      return nil
+    end
+    sum = 0
+    self.ratings.each do |rate|
+      sum += rate.score
+    end
+    avg = sum / self.ratings.count
+    return avg.to_f
+  end
 end
