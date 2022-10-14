df3aa60a76cc0db598293f68f89b236c2f8c8619
temejin <93370315+temejin@users.noreply.github.com>
2022-10-14 15:44:10 +0300

    Tehtävä 11

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   resources :beers
   resources :breweries
   root 'breweries#index'
-  get 'kaikki_bisset', to: 'beers#index'
-  get 'ratings', to: 'ratings#index'
-  get 'ratings/new', to:'ratings#new'
-  post 'ratings', to: 'ratings#create'
+  #get 'kaikki_bisset', to: 'beers#index'
+  #get 'ratings', to: 'ratings#index'
+  #get 'ratings/new', to:'ratings#new'
+  #post 'ratings', to: 'ratings#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
