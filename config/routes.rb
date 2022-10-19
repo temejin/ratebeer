671fcf0eeee728fafe432ab3b365e294c070ba9d
temejin <93370315+temejin@users.noreply.github.com>
2022-10-19 21:58:42 +0300

    Sisään- ja uloskirjautumislinkit, Tehtävä 4

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   root 'breweries#index'
   get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
   #get 'kaikki_bisset', to: 'beers#index'
   #get 'ratings', to: 'ratings#index'
   #get 'ratings/new', to:'ratings#new'
