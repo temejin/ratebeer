d748925b6abacaefffac79fd19b9cc70ddf32c2a
temejin <93370315+temejin@users.noreply.github.com>
2022-10-19 21:20:39 +0300

    Lisättiin käyttäjät; Tehtävä 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  resources :users
   resources :beers
   resources :breweries
+  resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   root 'breweries#index'
+  get 'signup', to: 'users#new'
   #get 'kaikki_bisset', to: 'beers#index'
   #get 'ratings', to: 'ratings#index'
   #get 'ratings/new', to:'ratings#new'
   #post 'ratings', to: 'ratings#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :ratings, only: [:index, :new, :create, :destroy]
 
   # Defines the root path route ("/")
   # root "articles#index"
