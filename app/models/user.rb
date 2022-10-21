19e1f37f0615bc716c25c444627e926972eee016
temejin <93370315+temejin@users.noreply.github.com>
2022-10-21 23:08:37 +0300

    Salasanat käyttäjille; tehtävä 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ApplicationRecord
   include RatingAverage
 
+  has_secure_password
+
+  validate :validate_password
   validates :username, uniqueness: true,
                        length: {minimum: 3,
                                 maximum: 30}
+
+  def validate_password
+    if !password.match('[A-Z]+')
+      errors.add(:password, "must contain at least one capital letter")
+    end
+    if !password.match('\d+')
+      errors.add(:password, "must contain at least one number")
+    end
+    if password.length < 4
+      errors.add(:password, "must be at least 4 characters long")
+    end
+  end
 
   has_many :ratings
   has_many :beers, through: :ratings
