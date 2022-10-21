bef8edde131fc289d2ab913406614ed019ace4be
temejin <93370315+temejin@users.noreply.github.com>
2022-10-21 08:41:05 +0300

    Tehtävä 8
    Panimon perustamisvuoden maksimiarvo haetaan nyt nykyistä päivämäärää
    käyttäen.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,10 +4,22 @@ class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  validate :validate_year
   validates :name, presence: true
-  validates :year, numericality: {greater_than_or_equal_to: 1040,
-                                  less_than_or_equal_to: 2022,
-                                  only_integer: true}
+
+  def validate_year
+    if !year
+      errors.add(:year, "can't be blank")
+    elsif year.is_a? Integer
+      if year > Time.now.year
+        errors.add(:year, "cannot be in the future")
+      elsif year < 1040
+        errors.add(:year, "must be greater than or equal to 1040")
+      end
+    else
+      errors.add(:year, "must be an integer between 1040 and #{Time.now.year}")
+    end
+  end
 
   def print_report
     puts name
