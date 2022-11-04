81349e0c4c382ab2795b52a047caee2570c0ce80
temejin <93370315+temejin@users.noreply.github.com>
2022-11-04 22:55:37 +0200

    Ulkoasua bootstrapin avulla

--- Gemfile
+++ Gemfile
@@ -87,3 +87,6 @@ end
 
 gem 'bcrypt', '~> 3.1.7'
 gem 'httparty'
+gem 'bootstrap', '~> 5.2.0'
+gem 'jquery-rails'
+gem 'mini_racer'
--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -2,7 +2,7 @@ module RatingAverage
   extend ActiveSupport::Concern
 
   def average_rating
-    return 0 if ratings.empty?
+    return 0.0 if ratings.empty?
 
     (ratings.reduce(0.0){ |sum, r| sum + r.score } / ratings.count.to_f).round(2)
   end
