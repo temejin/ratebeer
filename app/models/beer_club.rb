b5962068eca12694117c551eef5037f99e719100
temejin <93370315+temejin@users.noreply.github.com>
2022-11-17 20:08:55 +0200

    Napeille ja lomakkeille tyyliä

--- app/models/beer_club.rb
+++ app/models/beer_club.rb
@@ -2,6 +2,8 @@ class BeerClub < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :members, through: :memberships, source: :user
 
+  validates :name, presence: true
+
   def to_s
     name
   end
